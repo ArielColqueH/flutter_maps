85a5c5a075c00ac86675a8b0a02fc66c827f27e3
ArielColqueH <arielcolque444@gmail.com>
2021-04-14 14:25:23 -0400

    final maps example done

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:great_places_app/providers/great_places.dart';
 import 'package:great_places_app/screens/add_place_screen.dart';
+import 'package:great_places_app/screens/place_detail_screen.dart';
 import 'package:provider/provider.dart';
 
 class PlacesListScreen extends StatelessWidget {
@@ -41,7 +42,9 @@ class PlacesListScreen extends StatelessWidget {
                           title: Text(greatPlaces.items[i].title),
                           subtitle: Text(greatPlaces.items[i].location.address),
                           onTap: () {
-                            //got o detail page
+                            Navigator.of(context).pushNamed(
+                                PlaceDetailScreen.routeName,
+                                arguments: greatPlaces.items[i].id);
                           },
                         ),
                       ),
